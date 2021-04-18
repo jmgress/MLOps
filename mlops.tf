6845dcb37d159271c1f2897b26d5ee115bbfb069
jmgress <jmgress>
2021-04-18 12:14:44 -0400

    Added command and busybox

--- mlops.tf
+++ mlops.tf
@@ -14,10 +14,23 @@ provider "docker" {
 resource "docker_container" "pythontf" {
   image = docker_image.python.latest
   name  = "pythontf"
+  command = ["tail", "-f" , "/dev/null"]
+}
+
+resource "docker_container" "busybox" {
+  name = "busybox"
+  image = docker_image.busybox.latest
+  command = ["tail", "-f" , "/dev/null"]
+}
+
+resource "docker_image" "busybox"{
+  name = "busybox"
+  keep_locally = true
 }
 
 resource "docker_image" "python" {
   name = "python:latest"
 }
+
 
 
